e4090a52ba2e9e4914d3354525ec0a277d244f36
aura80 <96388713+aura80@users.noreply.github.com>
2022-01-30 00:42:50 +0200

    Tema 7 update

--- Tema_7/Tema__7.py
+++ Tema_7/Tema__7.py
@@ -1,4 +1,3 @@
-import os
 '''1. Write a Rectangle class in Python language, allowing you to build a rectangle with length and width attributes.
 Create a Perimeter() method to calculate the perimeter of the rectangle and a Area() method to calculate the area of ​​the rectangle.
 Create a method display() that display the length, width, perimeter and area of an object created using an instantiation on rectangle class.
